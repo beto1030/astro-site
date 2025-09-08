ba2eaf434482899466928420cdab96f9c67b6a4e
Alberto Alvarado <albertoalvarado@MacBookPro.attlocal.net>
2025-09-08 14:38:55 -0700

    had two astro config files that was causing a issue. deleted one. and fixed the href links to use astro syntax

--- abo-study/astro.config.ts
+++ abo-study/astro.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // If deploying to https://USERNAME.github.io/REPO_NAME
   base: process.env.CI ? '/astro-site/' : '/',
-  site: 'https://beto1030.github.io/astro-site/', // optional but recommended
-  // Optional but recommended:
-  // Astro defaults to SSG; no need to set output unless you changed it.
+  site: 'https://beto1030.github.io/astro-site/',
+  trailingSlash: 'always',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
 
