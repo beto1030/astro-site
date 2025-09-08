c15ac8d4ffa547c80db1db15fd2a5a3cb16958c9
Alberto Alvarado <albertoalvarado@MacBookPro.attlocal.net>
2025-09-08 02:44:14 -0700

    fixed astro.config.ts file

--- abo-study/astro.config.ts
+++ abo-study/astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // If deploying to https://USERNAME.github.io/REPO_NAME
-  base: process.env.CI ? '/REPO_NAME/' : '/',
+  base: process.env.CI ? '/astro-site/' : '/',
   // Optional but recommended:
   // site: 'https://USERNAME.github.io/REPO_NAME',
   // Astro defaults to SSG; no need to set output unless you changed it.
